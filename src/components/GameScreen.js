5426579f2e389881cb339ccfc468ed6723faf8ea
S-Maitland <scott@maitland.me.uk>
2019-06-25 16:30:34 +0100

    update with game logic for gamescreen

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -5,17 +5,61 @@ import './Game.css';
 class GameScreen extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      playerHealth: this.props.player.health,
+      playerTurn: true,
+      enemyHealth: this.props.enemy.health,
+      enemyTurn: false
+    }
+
+    this.handlePlayerPunchAttack = this.handlePlayerPunchAttack.bind(this);
+    this.handleEnemyAttack = this.handleEnemyAttack.bind(this);
+    this.handlePlayerTurn = this.handlePlayerTurn.bind(this);
+    this.handleEnemyTurn = this.handleEnemyTurn.bind(this);
   }
 
-  componentDidMount(){
-    
+  handlePlayerTurn = () => {
+    this.setState({playerTurn: false})
+    this.setState({enemyTurn: true})
+  }
+
+  handleEnemyTurn = () => {
+    this.setState({playerTurn: true})
+    this.setState({enemyTurn: false})
+  }
+
+  handlePlayerPunchAttack = () => {
+    if(this.state.playerTurn === true){
+      this.setState({enemyHealth: (this.state.enemyHealth - 10)})
+      this.handlePlayerTurn()
+    }
+  }
+
+  handleEnemyAttack = () => {
+    if(this.state.enemyTurn === true){
+      this.setState({playerHealth: (this.state.playerHealth - 10)})
+      this.handleEnemyTurn()
+    }
   }
 
   render(){
     return(
       <div className= "gameScreenBackground">
-        <p>{this.props.player.name}</p>
-        <p>{this.props.enemy.name}</p>
+        <div>
+          <label> Player Name:{this.props.player.name}</label>
+          <label> Player Health:{this.state.playerHealth}</label>
+          <button onClick={this.handlePlayerPunchAttack}>
+            <label>DRAGON PUNCH</label>
+          </button>
+        </div>
+        <hr/>
+        <div>
+          <label>Enemy Name: {this.props.enemy.name}</label>
+          <label>Enemy Health: {this.state.enemyHealth}</label>
+          <button onClick={this.handleEnemyAttack}>
+            <label>ATTACK</label>
+          </button>
+        </div>
       </div>
     );
   }
